4f1bf140d17f687f68a80c6d473a5c7d3b64da69
Zul Fadhli Ibrahim <zulfbi7@gmail.com>
2015-03-07 05:09:29 +0800

    a

---
+++ R/ezmet.R
@@ -0,0 +1,18 @@
+
+
+date <- function(start, end) {
+  
+         
+    
+        date = seq(as.POSIXct(paste(start), tz = "GMT"), 
+                 as.POSIXct(paste(end), tz = "GMT"), by="hour")
+        
+        date = data.frame(date)
+        
+        colnames(date)= ("date")
+        
+   
+
+  
+  return(print(date))
+}
